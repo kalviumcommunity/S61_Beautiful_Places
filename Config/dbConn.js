db9426dc607d951605d8ce5ae463ee2846ed63ee
Chumani Lalasa <chumanilalasa@gmail.com>
2024-03-14 14:37:47 +0530

    URI in string

--- Config/dbConn.js
+++ Config/dbConn.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 const connectDB = async() => {
     try{
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        })
+        await mongoose.connect(process.env.DATABASE_URI)
     }catch(err){
         console.error(err);
     }
