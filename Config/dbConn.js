dc94e1297a1cac594273ada608a0811888653493
Chumani Lalasa <chumanilalasa@gmail.com>
2024-03-14 14:07:27 +0530

    connected to mongoDB

---
+++ Config/dbConn.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+    }catch(err){
+        console.error(err);
+    }
+}
+module.exports = connectDB
