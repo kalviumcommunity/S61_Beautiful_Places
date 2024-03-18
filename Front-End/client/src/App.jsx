80499c78bf518c39fcb57f47f35feeb6a8a38366
Chumani Lalasa <chumanilalasa@gmail.com>
2024-03-18 10:44:17 +0530

    create vite app

---
+++ Front-End/client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LandingPage from './Components/LandingPage'
+
+function App() {
+
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ Front-End/client/src/Components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function LandingPage(){
+    return(
+        <div>
+            <h1>Beautiful Places</h1>
+            <p>
+            Discover breathtaking landscapes, serene vistas, and hidden gems around the world all in one place. From snow-capped mountains to sun-kissed beaches, we've curated a collection of the most stunning places on Earth. Immerse yourself in the beauty of nature and let your wanderlust soar. Whether you seek adventure or tranquility, our curated list has something for everyone. Join us in exploring the wonders of the world and get inspired to embark on your next journey with BeautifulPlaces!
+            </p>
+        </div>
+    )
+}
+
+export default LandingPage;
